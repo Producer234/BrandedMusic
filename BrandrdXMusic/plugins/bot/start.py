2586416bff860f5a08d0432d7ae60d4e33aa54e9
Producer234 <prexplanation2@gmail.com>
2025-11-27 10:58:23 +0600

    Update start.py

--- BrandrdXMusic/plugins/bot/start.py
+++ BrandrdXMusic/plugins/bot/start.py
@@ -117,7 +117,7 @@ async def start_pm(client, message: Message, _):
 
             await lols.edit_text("**⚡ѕтαятιиg.**")
             await lols.edit_text("**⚡ѕтαятιиg....**")
-            m = await message.reply_sticker("CAACAgUAAxkBAAEQI1BlTLmx7PtOO3aPNshEU2gCy7iAFgACNQUAApqMuVeA6eJ50VbvmDME")
+            m = await message.reply_sticker("CAACAgUAAxkBAAEP4YRpJ9o8mF54aARYQNMcW_dCmw68gwACEQ8AAtaPGFRxp_MIy2mCsDYE")
             if message.chat.photo:
 
                 userss_photo = await app.download_media(
