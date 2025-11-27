c76eb8ea22dfad30a129d6aea32eb64d84f79ba5
Producer234 <prexplanation2@gmail.com>
2025-11-27 09:00:40 +0600

    Update Telegram.py

--- BrandrdXMusic/platforms/Telegram.py
+++ BrandrdXMusic/platforms/Telegram.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 import time
-from typing import Union
+from typing import Union, Optional
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
